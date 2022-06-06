fa0be856cb922353d39575975e9ef6d5fdefc025
Cyril Tiger <11755096+Allandrow@users.noreply.github.com>
2022-06-06 15:38:45 +0200

    :sparkles: Add Contributing / Feedback routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import { Layout } from '@layout';
-import { Route, Routes } from 'react-router-dom';
 import { lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Layout } from '@layout';
+import { Contributing, Feedback } from '@meta';
 
 const Home = lazy(() => import('@home'));
 const Result = lazy(() => import('@result'));
@@ -11,6 +12,8 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="acronym/:id" element={<Result />} />
+        <Route path="contributing" element={<Contributing />} />
+        <Route path="feedback" element={<Feedback />} />
       </Route>
     </Routes>
   );
--- src/modules/common/Header.tsx
+++ src/modules/common/Header.tsx
@@ -1,5 +1,9 @@
 function Header() {
-  return <header>Hello header</header>;
+  return (
+    <header>
+      <h1>Web Dev Acronyms</h1>
+    </header>
+  );
 }
 
 export default Header;
