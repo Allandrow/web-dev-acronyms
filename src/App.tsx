8298f2c4cc13870e8e5793e1b9897334291d3a86
Cyril Tiger <11755096+Allandrow@users.noreply.github.com>
2022-06-06 11:50:22 +0200

    :sparkles: Add Result Component + Route

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { Layout } from '@layout';
-import Home from '@home';
 import { Route, Routes } from 'react-router-dom';
+import { lazy } from 'react';
+
+const Home = lazy(() => import('@home'));
+const Result = lazy(() => import('@result'));
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
+        <Route path="acronym/:id" element={<Result />} />
       </Route>
     </Routes>
   );
---
+++ src/modules/result/Result.tsx
@@ -0,0 +1,9 @@
+function Result() {
+  return (
+    <main>
+      <h1>Result</h1>
+    </main>
+  );
+}
+
+export default Result;
