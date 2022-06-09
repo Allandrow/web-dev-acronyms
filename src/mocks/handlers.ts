516f40bd8302a3c665578dba46c6606521cf1439
Cyril Tiger <11755096+Allandrow@users.noreply.github.com>
2022-06-09 12:02:52 +0200

    :sparkes: Add basic result content from acronym query

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,11 +1,26 @@
 import { rest } from 'msw';
-import { acronymList } from '@data';
+import { acronymList, acronymsDetails } from '@data';
 
 const list = acronymList.sort().map((acronym) => acronym.toLowerCase());
 
 const handlers = [
   rest.get('/acronyms', (_, res, ctx) => {
     return res(ctx.status(200), ctx.json(list));
+  }),
+
+  rest.get('/acronym/:id', (req, res, ctx) => {
+    const { id } = req.params;
+
+    const acronym = acronymsDetails.find((item) => item.acronym === id);
+
+    if (!acronym) {
+      return res(
+        ctx.status(404),
+        ctx.json({ message: `Acronym ${id} not found.` }),
+      );
+    }
+
+    return res(ctx.status(200), ctx.json(acronym));
   }),
 ];
 
--- src/modules/result/Result.tsx
+++ src/modules/result/Result.tsx
@@ -1,7 +1,40 @@
+import { useQuery } from 'react-query';
+import { Link, useParams } from 'react-router-dom';
+import { Acronym } from '@data';
+
 function Result() {
+  const { id } = useParams();
+
+  const { data } = useQuery<Acronym>(['acronyms', id], () =>
+    fetch(`/acronym/${id}`).then((res) => res.json()),
+  );
+
   return (
     <main>
-      <h1>Result</h1>
+      <p>
+        {data?.acronym} means {data?.definition}
+      </p>
+      <h3>Quick description</h3>
+      <p>{data?.quote}</p>
+      <p>Quote from {data?.quoteAuthor}</p>
+      <h3>Further learning</h3>
+      <ul>
+        {data?.resources.map((media) => (
+          <li key={media.title}>
+            <a href={media.url}>
+              <span>{media.type}</span> - <span>{media.title}</span>
+            </a>
+          </li>
+        ))}
+      </ul>
+      <h3>Related acronyms</h3>
+      <ul>
+        {data?.related.map((item) => (
+          <li key={item}>
+            <Link to={`/acronym/${item}`}>{item}</Link>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
