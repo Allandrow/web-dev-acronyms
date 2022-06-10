0409723cfc329a43b5ca32500857fece732666a9
Cyril Tiger <11755096+Allandrow@users.noreply.github.com>
2022-06-10 16:12:14 +0200

    :rewind: Remove MUI

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,7 @@ import ReactDOM from 'react-dom/client';
 import worker from 'mocks/browser';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { CssBaseline } from '@mui/material';
 import App from './App';
-
-// MUI FONT IMPORT
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
 
 // MSW WORKER
 worker.start({
@@ -30,22 +22,12 @@ const queryClient = new QueryClient({
   },
 });
 
-// MUI Theme
-const muiDarkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-  },
-});
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider theme={muiDarkTheme}>
-      <CssBaseline />
-      <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </QueryClientProvider>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>,
 );
