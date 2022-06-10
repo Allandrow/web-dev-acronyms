62b7d1e3431d1a1580a854a24e8b00ffa285af6c
Cyril Tiger <11755096+Allandrow@users.noreply.github.com>
2022-06-10 12:27:55 +0200

    :sparkles: Add Link in header

--- src/modules/common/Header.tsx
+++ src/modules/common/Header.tsx
@@ -1,7 +1,11 @@
+import { Link } from 'react-router-dom';
+
 function Header() {
   return (
     <header>
-      <h1>Web Dev Acronyms</h1>
+      <Link to="/">
+        <h1>Web Dev Acronyms</h1>
+      </Link>
     </header>
   );
 }
